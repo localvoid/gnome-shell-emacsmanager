686b18fdb91ce8a5965b4565acf03779fc07f916
Boris Kaul <localvoid@gmail.com>
2012-11-24 10:22:48 +0700

    fix uid

--- settings.js
+++ settings.js
@@ -1,15 +1,8 @@
 // -*- mode: js; flymake-mode: -1; js-indent-level: 4; indent-tabs-mode: nil -*-
 const GLib = imports.gi.GLib;
 
-let UID;
-
-(function() {
-    let ret = GLib.spawn_sync(null, ['/usr/bin/id', '-u'], null, 0, null);
-    UID = new String(ret[1]).trim();
-}());
-
 const EMACS_DIR = GLib.build_filenamev([GLib.get_home_dir(), '.emacs.d'])
     , EMACS_DESKTOP_DIR = GLib.build_filenamev([EMACS_DIR, 'desktop'])
-    , EMACS_SOCKETS_DIR = GLib.build_filenamev(['/', 'tmp', 'emacs' + UID])
+    , EMACS_SOCKETS_DIR = GLib.build_filenamev(['/', 'tmp', 'emacs' + imports.system.getuid()])
     , EMACS_SERVERS_DIR = GLib.build_filenamev([EMACS_DIR, 'server'])
     , EMACS_VIRTUAL_DIR = GLib.build_filenamev([EMACS_DIR, 'virtualenv']);
